e48c2568ee66c32e1147f3feccac19bf49623009
Eli Rooney <31978743+EliRooney@users.noreply.github.com>
2018-09-26 09:11:54 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -4,7 +4,7 @@ var x = document.querySelectorAll("p");
 console.log(x);
 
 // const hi = () => {
-// 	console.log('Whats up bitch') ;
+// 	console.log('Whats up') ;
 // }
 
 // elem1.addEventListener("click", hi()); 
